f55a9364f3e6db68f36efd30cf5f45afc0c23c6a
csj <1311256696@qq.com>
2019-04-08 18:27:57 +0800

    Mahout

---
+++ mahout/src/main/java/com/rmjk/mahout/jobs/MyIRStatistics.java
@@ -0,0 +1,44 @@
+package com.rmjk.job.jobs;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.IRStatistics;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.cf.taste.similarity.precompute.example.GroupLensDataModel;
+
+import java.io.File;
+
+/**
+ * Created by csj
+ * 获取推荐的准确率和召回率
+ */
+public class MyIRStatistics {
+    public static void main(String[] args) throws Exception {
+        //准备数据 这里是电影评分数据
+        File file = new File("E:\\ml-10M100K\\ratings.dat");
+        //将数据加载到内存中，GroupLensDataModel是针对开放电影评论数据的
+        DataModel dataModel = new GroupLensDataModel(file);
+        RecommenderIRStatsEvaluator statsEvaluator = new GenericRecommenderIRStatsEvaluator();
+        RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
+            public Recommender buildRecommender(DataModel model) throws TasteException {
+                UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+                UserNeighborhood neighborhood = new NearestNUserNeighborhood(4, similarity, model);
+                return new GenericUserBasedRecommender(model, neighborhood, similarity);
+            }
+        };
+        // 计算推荐4个结果时的查准率和召回率
+        //使用评估器，并设定评估期的参数
+        //4表示"precision and recall at 4"即相当于推荐top4，然后在top-4的推荐上计算准确率和召回率
+        IRStatistics stats = statsEvaluator.evaluate(recommenderBuilder, null, dataModel, null, 4, GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD, 1.0);
+        System.out.println(stats.getPrecision());
+        System.out.println(stats.getRecall());
+    }
+}
