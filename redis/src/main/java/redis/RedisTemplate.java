a125bbc507bb2a52db472c08ec73cbe643346f05
csj <1311256696@qq.com>
2019-04-09 11:26:58 +0800

    redis

---
+++ redis/src/main/java/redis/RedisTemplate.java
@@ -0,0 +1,40 @@
+package com.hys.cms.Utils.redis;
+
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
+
+/**
+ * @ClassName: RedisTemplate
+ * @Auther: csj
+ * @Description: 重写RedisTemplate,加入选库
+ */
+public class RedisTemplate extends org.springframework.data.redis.core.RedisTemplate {
+
+    public static ThreadLocal<Integer> indexdb = new ThreadLocal<Integer>(){
+        @Override protected Integer initialValue() { return 0; }
+    };
+
+    @Override
+    protected RedisConnection preProcessConnection(RedisConnection connection, boolean existingConnection) {
+        try {
+            Integer dbIndex = indexdb.get();
+            //如果设置了dbIndex
+            if (dbIndex != null) {
+                if (connection instanceof JedisConnection) {
+                    if (((JedisConnection) connection).getNativeConnection().getDB().intValue() != dbIndex) {
+                        connection.select(dbIndex);
+                    }
+                } else {
+                    connection.select(dbIndex);
+                }
+            } else {
+                connection.select(0);
+            }
+        } finally {
+            indexdb.remove();
+        }
+        return super.preProcessConnection(connection, existingConnection);
+    }
+
+
+}
---
+++ redis/src/main/java/redis/RedisTemplateConfig.java
@@ -0,0 +1,147 @@
+package com.hys.cms.Utils.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * @ClassName: RedisConfig
+ * @Auther: csj
+ * @Date: 2018/8/28 11:07
+ * @Description:
+ */
+@Configuration
+//@ConfigurationProperties(prefix = "redis")
+public class RedisTemplateConfig {
+    private static final Logger log = LoggerFactory.getLogger(RedisTemplateConfig.class);
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.password}")
+    private String password;
+
+    @Value("${redis.port}")
+    private Integer port;
+
+    @Value("${redis.maxIdle}")
+    private Integer maxIdle;
+
+    @Value("${redis.timeout}")
+    private Integer timeout;
+
+    @Value("${redis.maxTotal}")
+    private Integer maxTotal;
+
+    @Value("${redis.maxWaitMillis}")
+    private Integer maxWaitMillis;
+
+    @Value("${redis.minEvictableIdleTimeMillis}")
+    private Integer minEvictableIdleTimeMillis;
+
+    @Value("${redis.numTestsPerEvictionRun}")
+    private Integer numTestsPerEvictionRun;
+
+    @Value("${redis.timeBetweenEvictionRunsMillis}")
+    private long timeBetweenEvictionRunsMillis;
+
+    @Value("${redis.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${redis.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    /**
+     * @auther: csj
+     * @date: 17:52 2018/8/28
+     * @param: []
+     * @return: org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+     * @Description: Jedis配置
+     */
+    @Bean
+    public JedisConnectionFactory JedisConnectionFactory(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration ();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        //由于我们使用了动态配置库,所以此处省略
+        //redisStandaloneConfiguration.setDatabase(database);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+        jedisClientConfiguration.connectTimeout(Duration.ofMillis(timeout));
+        JedisConnectionFactory factory = new JedisConnectionFactory(redisStandaloneConfiguration,
+                jedisClientConfiguration.build());
+        return factory;
+    }
+
+    /**
+     * @auther: csj
+     * @param: [redisConnectionFactory]
+     * @return: com.springboot.demo.base.utils.RedisTemplate
+     * @Description: 实例化 RedisTemplate 对象
+     */
+    @Bean
+    public RedisTemplate functionDomainRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("RedisTemplate实例化成功！");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        initDomainRedisTemplate(redisTemplate, redisConnectionFactory);
+        return redisTemplate;
+    }
+
+    /**
+     * @auther: csj
+     * @date: 17:51 2018/8/28
+     * @param: [redisTemplate, factory]
+     * @return: void
+     * @Description: 设置数据存入 redis 的序列化方式,并开启事务
+     */
+    private void initDomainRedisTemplate(RedisTemplate redisTemplate, RedisConnectionFactory factory) {
+        //如果不配置Serializer，那么存储的时候缺省使用String，如果用User类型存储，那么会提示错误User can't cast to String！
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(fastJson2JsonRedisSerializer());
+        // 开启事务
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.setConnectionFactory(factory);
+    }
+
+    /**
+     * @auther: csj
+     * @date: 17:52 2018/8/28
+     * @param: []
+     * @return: org.springframework.data.redis.serializer.RedisSerializer
+     * @Description: 引入自定义序列化
+     */
+    @Bean
+    public RedisSerializer fastJson2JsonRedisSerializer() {
+        return new FastJson2JsonRedisSerializer<Object>(Object.class);
+    }
+
+    /**
+     * @auther: csj
+     * @date: 17:51 2018/8/28
+     * @param: [redisTemplate]
+     * @return: com.springboot.demo.base.utils.RedisUtil
+     * @Description: 注入封装RedisTemplate
+     */
+    @Bean(name = "redisUtil")
+    public RedisTemplateUtil redisUtil(RedisTemplate redisTemplate) {
+        log.info("RedisUtil注入成功！");
+        RedisTemplateUtil redisTemplateUtil = new RedisTemplateUtil();
+        redisTemplateUtil.setRedisTemplate(redisTemplate);
+        return redisTemplateUtil;
+    }
+
+}
